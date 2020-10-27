dbb00472feb7a91a893df0f126d4bcd0290af547
Fred Berchof <fredberchof@MacBook-Pro-de-bruno.local>
2020-10-27 16:32:06 -0300

    Grades_Control_React

---
+++ front/src/App.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+
+import * as api from "./api/apiService";
+import Spiner from "./components/Spiner";
+import GradesControl from "./components/GradesControl";
+import ModalGrade from "./components/ModalGrade";
+
+export default function App() {
+  const [allGrades, setAllGrades] = useState([]);
+  const [selectedGrade, setSelectedGrade] = useState({});
+  const [isModalOpen, setIsmodalOpen] = useState(false);
+
+  useEffect(() => {
+    const getGrades = async () => {
+      const grades = await api.getAllGrades();
+      setTimeout(() => {
+        setAllGrades(grades);
+      }, 1000);
+    };
+
+    // api.getAllGrades().then((grades) => {
+    //   setTimeout(() => {
+    //     setAllGrades(grades)
+    //   }, 1000)
+    // })
+
+    getGrades();
+  }, []);
+
+  const handleDelete = async (gradeToDelete) => {
+    const isDeleted = await api.deleteGrade(gradeToDelete);
+
+    if (isDeleted) {
+      const deletedGradeIndex = allGrades.findIndex(
+        (grade) => grade.id === gradeToDelete.id
+      );
+
+      const newGrades = Object.assign([], allGrades);
+      newGrades[deletedGradeIndex].isDeleted = true;
+      newGrades[deletedGradeIndex].value = 0;
+
+      setAllGrades(newGrades);
+    }
+  };
+
+  const handlePersist = (grade) => {
+    setSelectedGrade(grade);
+    setIsmodalOpen(true);
+  };
+
+  const handlePersistData = async (formData) => {
+    const { id, newValue } = formData;
+
+    const newGrades = Object.assign([], allGrades);
+
+    const gradeToPersist = newGrades.find((grade) => grade.id === id);
+    gradeToPersist.value = newValue;
+
+    if (gradeToPersist.isDeleted) {
+      gradeToPersist.isDeleted = false;
+      await api.insertGrade(gradeToPersist);
+    } else {
+      await api.updateGrade(gradeToPersist);
+    }
+
+    setIsmodalOpen(false);
+  };
+
+  const handleClose = () => {
+    setIsmodalOpen(false);
+  };
+
+  return (
+    <div>
+      <h1 className="center">Controle de notas</h1>
+
+      {allGrades.length === 0 && <Spiner />}
+      {allGrades.length > 0 && (
+        <GradesControl
+          grades={allGrades}
+          onDelete={handleDelete}
+          onPersist={handlePersist}
+        />
+      )}
+
+      {isModalOpen && (
+        <ModalGrade
+          onSave={handlePersistData}
+          onClose={handleClose}
+          selectedGrade={selectedGrade}
+        />
+      )}
+    </div>
+  );
+}
